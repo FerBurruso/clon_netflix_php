1bc37c5e34c89bc83b7d0999f3444a5222f71355
FerBurruso <burrusof@gmail.com>
2022-07-22 02:31:03 -0300

    subida inicial

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css"> 
+    <title>Clon_Netflix</title>
+</head>
+<body>
+    <div class="contenedor_principal">
+        <div class="contenedor_gradiente">
+            <header class="seccion_header">
+                <div class="logo">
+                    <img src="images/Logo_netflix.png" alt="">
+                </div>
+                <div>
+                    <select class="combo_idioma" >
+                        <option>Español</option>
+                        <option>Inglés</option>
+                    </select>
+                </div>
+                <div>
+                    <input class="boton_rojo" type="button" value="Iniciar sesión">
+                </div>
+            </header>
+            <div class="story_card_externo">
+                <div class="story_card">
+                    <div class="titulo_principal">
+                        <h1>
+                            <p>Películas y series ilimitadas</p>
+                            <p>y mucho más.</p>
+                        </h1>
+                    </div>
+                    <div class="titulo_secundario">
+                        <h2>
+                            <p>Disfruta donde quieras. Cancela cuando quieras.</p>
+                        </h2>
+                    </div>
+                    <div class="contenedor_mail">
+                        <h3>
+                            <p>¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta o reiniciar tu membresía de Netflix.</p>
+                        </h3>
+                        <div class="subcontenedor_mail">
+                            <input class="input_mail"type="email" name="mail" id="mail" placeholder="Email">
+                            <input class="boton_mail" type="button" value="Comenzar >">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="perfiles_ninos">
+        <img class="imagen_ninos" src="images/ninos.png" alt="" >
+    
+        <div class="texto_ninos">
+            <h2>Crea perfiles para niños.</h2><br>
+            <p>Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.</p>
+        </div>
+        </div>
+        <footer>
+            <div class="footer_top">
+                <p>¿Preguntas? Llama al 0-800-666-2803</p><br>
+            </div>
+            <nav class="footer_columnas"> 
+                <a class="footer_link" href="">Preguntas frecuentes</a>
+                <a class="footer_link" href="">Centro de ayuda</a>
+                <a class="footer_link" href="">Cuenta</a>
+                <a class="footer_link" href="">Prensa</a>
+                <a class="footer_link" href="">Relaciones con inversionistas</a>
+                <a class="footer_link" href="">Empleo</a>
+                <a class="footer_link" href="">Formas de ver</a>
+                <a class="footer_link" href="">Términos de uso</a>
+                <a class="footer_link" href="">Privacidad</a>
+                <a class="footer_link" href="">Preferencia de cookies</a>
+                <a class="footer_link" href="">Información corporativa</a>
+                <a class="footer_link" href="">Contáctanos</a>
+                <a class="footer_link" href="">Prueba de velocidad</a>
+                <a class="footer_link" href="">Avisos legales</a>
+                <a class="footer_link" href="">Solo en Netflix</a>
+            </nav>
+        </footer>
+</body>
+</html>
+
